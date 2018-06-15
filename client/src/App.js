e12dab66ee4b68fa10868dcaf79d2bb431d3b9fd
Bradiowave <divebombwalker@gmail.com>
2018-06-15 10:21:12 -0700

    connected cors and initialized client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hola muchachos
+      </div>
+    );
+  }
+}
+
+export default App;
